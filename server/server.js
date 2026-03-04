67c00fb7fca51417ae43903597ea850335435589
vektiscc <59166041+vektiscc@users.noreply.github.com>
2026-03-04 22:54:14 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,76 +7,53 @@ const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
-
 const io = new Server(server, {
-  cors: { origin: "*" }
+  cors: {
+    origin: "*"
+  }
 });
 
-const users = {};
+const users = {}; // userId -> { nickname, socketId }
 
 io.on("connection", (socket) => {
+  console.log("Connected:", socket.id);
 
   socket.on("join", ({ userId, nickname }) => {
-
-    const nicknameTaken = Object.values(users).some(
-      u => u.nickname === nickname && u.socketId
-    );
-
-    if (nicknameTaken) {
-      socket.emit("nickname_taken");
-      return;
-    }
-
     users[userId] = {
       nickname,
       socketId: socket.id
     };
 
-    socket.userId = userId;
-    socket.nickname = nickname;
-
     io.emit("users_update", users);
   });
 
-  socket.on("typing", ({ room, to }) => {
-    io.to(room).emit("typing", {
-      from: socket.userId
-    });
-  });
-
-  socket.on("stop_typing", ({ room }) => {
-    io.to(room).emit("stop_typing", {
-      from: socket.userId
-    });
-  });
-
-  socket.on("start_dialog", ({ room }) => {
-    socket.join(room);
-  });
-
-  socket.on("send_message", (data) => {
-    io.to(data.room).emit("receive_message", {
-      ...data,
-      authorId: socket.userId,
-      authorName: socket.nickname
-    });
-  });
-
-  socket.on("delete_message", ({ room, messageId }) => {
-    io.to(room).emit("message_deleted", { room, messageId });
-  });
-
-  socket.on("delete_chat", ({ room }) => {
-    io.to(room).emit("chat_deleted", { room });
-  });
-
-  socket.on("disconnect", () => {
-    if (socket.userId && users[socket.userId]) {
-      users[socket.userId].socketId = null;
-      io.emit("users_update", users);
+  socket.on("create_dialog", ({ fromUserId, toUserId, fromNickname }) => {
+    if (users[toUserId]) {
+      io.to(users[toUserId].socketId).emit("dialog_created", {
+        userId: fromUserId,
+        nickname: fromNickname
+      });
     }
   });
 
+  socket.on("send_message", ({ fromUserId, toUserId, message, time }) => {
+    if (users[toUserId]) {
+      io.to(users[toUserId].socketId).emit("receive_message", {
+        fromUserId,
+        message,
+        time
+      });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    for (const id in users) {
+      if (users[id].socketId === socket.id) {
+        delete users[id];
+      }
+    }
+    io.emit("users_update", users);
+  });
 });
 
 server.listen(3001, () => {
